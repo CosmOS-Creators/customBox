6bdeca1783fb0684d08801ac45acffdf73b8c789
florianL21 <flo.laschober@gmail.com>
2021-06-16 18:34:54 +0200

    Config parser is now creating an object structure instead of a dictionary Also cleaned up the code a bit

--- newGenerator/ConfigParser.py
+++ newGenerator/ConfigParser.py
@@ -1,7 +1,6 @@
 import json
 import os
-
-configInPath = r"C:\Files\Projects\Developement\CosmOS\reference_project_stmIDE\Cosmos\generated\integration\systemConfig\configDef"
+from types import SimpleNamespace
 
 ELEMENTS_KEY    = "elements"
 ATTRIBUTES_KEY  = "attributes"
@@ -12,44 +11,64 @@ VALUE_KEY       = "value"
 
 PARENT_REFERENCE_KEY = "parentReference"
 
-
-
-
-PARENT_REFERENCE_PLACEHOLDER_KEY = "parentReferenceNeedsToBeLinked"
+OBJECT_ID_ATTRIBUTE = "id"
+OBJECT_ITERATOR_ATTRIBUTE = "iterator"
 
 required_json_config_keys = [ELEMENTS_KEY, ATTRIBUTES_KEY, VERSION_KEY]
 
-configuration = {}
-
-
-def processConfig(config, configName):
-    global configuration
+def processConfig(config: dict, configName: str, completeConfig: object):
     for element in config[ELEMENTS_KEY]:
         currentElement = config[ELEMENTS_KEY][element]
-        if(not configName in configuration):
-            configuration[configName] = {}
-        configuration[configName][element] = {}
+        if(not hasattr(completeConfig, configName)):
+            setattr(completeConfig, configName, SimpleNamespace())
+            currentConfig = getattr(completeConfig, configName)
+            setattr(currentConfig, OBJECT_ITERATOR_ATTRIBUTE, [])
+        else:
+            currentConfig = getattr(completeConfig, configName)
+
+        newElement = SimpleNamespace()
+        setattr(currentConfig, element, newElement)
+        setattr(newElement, OBJECT_ID_ATTRIBUTE, element) # add the key of the element as an id inside the object so that it can be accessed also when iterating over the elements
+        iterator = getattr(currentConfig, OBJECT_ITERATOR_ATTRIBUTE)
+        iterator.append(newElement)
         for attributeInstance in currentElement:
             if(TARGET_KEY in attributeInstance and VALUE_KEY in attributeInstance): # this is a normal attribute instance
-                configuration[configName][element][attributeInstance[TARGET_KEY]] = attributeInstance[VALUE_KEY]
-            elif(PARENT_REFERENCE_KEY in attributeInstance): # this is parent reference special attribute instance
-                configuration[configName][element][PARENT_REFERENCE_PLACEHOLDER_KEY] = attributeInstance[PARENT_REFERENCE_KEY]
-            else:
+                setattr(newElement, attributeInstance[TARGET_KEY], attributeInstance[VALUE_KEY])
+            elif(not PARENT_REFERENCE_KEY in attributeInstance): # this is parent reference special attribute instance
                 raise Exception(f"Invalid attribute instance formatting in {configName} config. The following property is invalid: {attributeInstance}")
 
+def resolveElementLink(configObj: object, link: str):
+    config, target = link.split('/')
+    targetConfig = getattr(configObj, config)
+    return getattr(targetConfig, target)
 
-def linkParents(config):
-    pass
+def linkParents(jsonConfigs: dict, objConfigs: object):
+    for config in jsonConfigs:
+        for element in jsonConfigs[config][ELEMENTS_KEY]:
+            for attributeInstance in jsonConfigs[config][ELEMENTS_KEY][element]:
+                if(PARENT_REFERENCE_KEY in attributeInstance):
+                    parentObject = resolveElementLink(objConfigs, attributeInstance[PARENT_REFERENCE_KEY])
+                    link = SimpleNamespace()
+                    if(not hasattr(parentObject, config)):
+                        setattr(parentObject, config, link)
+                        config_obj = getattr(objConfigs, config)
+                        parentLink_obj = getattr(parentObject, config)
+                        setattr(parentLink_obj, OBJECT_ITERATOR_ATTRIBUTE, [])
+                    else:
+                        config_obj = getattr(objConfigs, config)
+                        parentLink_obj = getattr(parentObject, config)
+                    element_obj = getattr(config_obj, element)
+                    setattr(parentLink_obj, element, element_obj)
+                    iterator = getattr(parentLink_obj, OBJECT_ITERATOR_ATTRIBUTE)
+                    iterator.append(element_obj)
 
-
-def config_file_sanity_check(config, filepath):
+def config_file_sanity_check(config: dict, filepath: str):
     for required_key in required_json_config_keys:
-        if(not (required_key in config)):
+        if(not required_key in config):
             raise KeyError(f"Every config file must have a key named {required_key} but the file \"{filepath}\" didn't")
 
-
 def loadConfig(configPath: str):
-    global configuration
+    configuration = SimpleNamespace()
     if(not os.path.exists(configPath) or not os.path.isdir(configPath)):
         raise FileNotFoundError("Input path was not valid or not a directory")
     configFiles = [f for f in os.listdir(configPath) if f.endswith('.json')]
@@ -63,10 +82,15 @@ def loadConfig(configPath: str):
             jsonConfigs[configCleanName] = loaded_json_config
 
     for config in jsonConfigs:
-        processConfig(jsonConfigs[config], config)
-    with open("dump", "w") as file:
-        json.dump(configuration,file)
-    linkParents(configuration)
-    None
+        processConfig(jsonConfigs[config], config, configuration)
+    linkParents(jsonConfigs, configuration)
+    return configuration
 
-loadConfig(configInPath)
+if __name__ == "__main__":
+    from pretty_simple_namespace import pprint
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("INPUT", help="Input config file path")
+    args = parser.parse_args()
+    fullConfig = loadConfig(args.INPUT)
+    pprint(fullConfig)
