748e8947e4c80822ceeb9ee2afb2f9cae7a08810
florianL21 <flo.laschober@gmail.com>
2021-07-11 21:12:30 +0200

    Moved generator files to a better place

--- newGenerator/Model/Features/MemoryMapper/privateClasses/chunk.py
+++
@@ -1,5 +0,0 @@
-class Chunk():
-    def __init__(self,paramLowAddress,paramHighAddress,paramSize):
-        self.highAddress = paramHighAddress
-        self.lowAddress = paramLowAddress
-        self.size = paramHighAddress - paramLowAddress
