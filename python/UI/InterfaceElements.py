cd036ede7d3b261505b095be6789213e561e0bcf
florianL21 <flo.laschober@gmail.com>
2021-08-16 17:42:48 +0200

    Labels are now all aligned

--- python/UI/InterfaceElements.py
+++ python/UI/InterfaceElements.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Union
 from PySide6.QtCore import QRegularExpression
 from PySide6.QtGui import QRegularExpressionValidator
 
@@ -11,17 +11,27 @@ class Ui_element(QWidget):
 	comparisonType = ""
 	def __init__(self, parent: QWidget, attribute: AttributeInstance, layout: QLayout):
 		super().__init__(parent)
-		self.attributeDef 	= attribute.attributeDefinition
-		self.Layout 		= layout
+		self.ui_element: QWidget	= None
+		self.label: QLabel			= None
+		self.attributeDef 			= attribute.attributeDefinition
+		self.Layout 				= layout
 		self.setLayout(self.Layout)
 		self.Layout.setContentsMargins(0,0,0,0)
+
+	def get_label_width(self):
+		self.label.adjustSize()
+		return self.label.width()
+
+	def set_label_width(self, label_width):
+		self.label.setMinimumWidth(label_width)
+		self.label.setMaximumWidth(label_width)
 
 class String_element(Ui_element):
 	comparisonType = "string"
 	def __init__(self, parent: QWidget, attribute: AttributeInstance):
 		super().__init__(parent, attribute, QHBoxLayout())
 		self.attributeDef: StringType
-		self.label = QLabel(self.attributeDef.label, parent)
+		self.label 		= QLabel(self.attributeDef.label, parent)
 		self.ui_element = QLineEdit(parent)
 		if(self.attributeDef.validation):
 			self.ui_element.setValidator(QRegularExpressionValidator(QRegularExpression(self.attributeDef.validation)))
@@ -34,8 +44,10 @@ class Bool_element(Ui_element):
 	def __init__(self, parent: QWidget, attribute: AttributeInstance):
 		super().__init__(parent, attribute, QHBoxLayout())
 		self.attributeDef: BoolType
-		self.ui_element = QCheckBox(self.attributeDef.label, parent)
+		self.label 		= QLabel(self.attributeDef.label, parent)
+		self.ui_element = QCheckBox(parent)
 		self.ui_element.setChecked(attribute.value)
+		self.Layout.addWidget(self.label, 0)
 		self.Layout.addWidget(self.ui_element, 1)
 
 class Int_element(Ui_element):
@@ -43,7 +55,7 @@ class Int_element(Ui_element):
 	def __init__(self, parent: QWidget, attribute: AttributeInstance):
 		super().__init__(parent, attribute, QHBoxLayout())
 		self.attributeDef: IntType
-		self.label = QLabel(self.attributeDef.label, parent)
+		self.label 		= QLabel(self.attributeDef.label, parent)
 		self.ui_element = QSpinBox(parent)
 		if(self.attributeDef.min):
 			self.ui_element.setMinimum(self.attributeDef.min)
@@ -59,7 +71,7 @@ class Float_element(Ui_element):
 	def __init__(self, parent: QWidget, attribute: AttributeInstance):
 		super().__init__(parent, attribute, QHBoxLayout())
 		self.attributeDef: FloatType
-		self.label = QLabel(self.attributeDef.label, parent)
+		self.label 		= QLabel(self.attributeDef.label, parent)
 		self.ui_element = QSpinBox(parent)
 		if(self.attributeDef.min):
 			self.ui_element.setMinimum(self.attributeDef.min)
@@ -75,7 +87,7 @@ class Selection_element(Ui_element):
 	def __init__(self, parent: QWidget, attribute: AttributeInstance):
 		super().__init__(parent, attribute, QHBoxLayout())
 		self.attributeDef: SelectionType
-		self.label = QLabel(self.attributeDef.label, parent)
+		self.label 		= QLabel(self.attributeDef.label, parent)
 		self.ui_element = QComboBox(parent)
 		if(type(self.attributeDef.elements) is list):
 			for selectionElement in self.attributeDef.elements:
@@ -92,7 +104,7 @@ class Selection_element(Ui_element):
 
 avaliable_ui_elements: List[Ui_element] = [String_element, Bool_element, Int_element, Float_element, Selection_element]
 
-def create_interface_element(parent: QWidget, attributeInstance: AttributeInstance):
+def create_interface_element(parent: QWidget, attributeInstance: AttributeInstance) -> Union[None, Ui_element]:
 	Attribute = attributeInstance.attributeDefinition
 	if(not Attribute.is_placeholder and not Attribute.hidden):
 		for ui_element in avaliable_ui_elements:
--- python/UI/PageBuilder.py
+++ python/UI/PageBuilder.py
@@ -1,29 +1,49 @@
-from typing import List, Tuple
+from typing import Dict, List, Tuple
 from PySide6.QtWidgets import QTabWidget, QVBoxLayout, QWidget
-from Parser.ConfigTypes import Configuration, Subconfig
-from UI.InterfaceElements import create_interface_element
+from Parser.ConfigTypes import Configuration, Subconfig, UiViewTypes
+from UI.InterfaceElements import Ui_element, create_interface_element
 
-def buildPage(subconfigs: dict[str, Subconfig], name: str):
+class card(QWidget):
+	def __init__(self, parent: QWidget):
+		super().__init__(parent)
+
+def buildTabbedPage(subconfigs: dict[str, Subconfig], name: str):
 	tabwidget 	= QTabWidget()
 	tabwidget.setObjectName(name)
 	for subconfig in subconfigs.values():
-		for element_name, element in subconfig.elements.items():
-			page 	= QWidget()
-			layout 	= QVBoxLayout()
-			page.setLayout(layout)
-			tabwidget.addTab(page, element_name)
-			for attributeInstance in element.attributeInstances.values():
-				new_widget = create_interface_element(page, attributeInstance)
-				if(new_widget):
-					layout.addWidget(new_widget)
-			layout.addStretch()
-
+		pages 	= buildPage(subconfig)
+		for page_name, page in pages:
+			tabwidget.addTab(page, page_name)
 	return tabwidget
+
+def buildPage(subconfig: Subconfig):
+	pages: List[Tuple[str, QWidget]] = list()
+	for element_name, element in subconfig.elements.items():
+		page 	= QWidget()
+		layout 	= QVBoxLayout()
+		page.setLayout(layout)
+		pages.append((element_name, page))
+		max_label_width 	= 0
+		widgets: List[Ui_element] = list()
+		for attributeInstance in element.attributeInstances.values():
+			new_widget = create_interface_element(page, attributeInstance)
+			if(new_widget):
+				widgets.append(new_widget)
+				layout.addWidget(new_widget)
+				label_width = new_widget.get_label_width()
+				if(max_label_width < label_width):
+					max_label_width = label_width
+		for widget in widgets:
+			widget.set_label_width(max_label_width)
+		layout.addStretch()
+	return pages
 
 def buildAllPages(config: Configuration):
 	pages: List[Tuple[QWidget, str]] = list()
 	for page_id, page in config.UiConfig.pages.items():
-		# page.viewType
-		new_page = buildPage(page.assignedSubconfigs, page.label)
-		pages.append((new_page, page.icon))
+		if(page.viewType == UiViewTypes.tabbed):
+			new_page = buildTabbedPage(page.assignedSubconfigs, page.label)
+			pages.append((new_page, page.icon))
+		elif(page.viewType == UiViewTypes.tabbed):
+			pass
 	return pages
